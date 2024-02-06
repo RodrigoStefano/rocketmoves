ee754066ceebac898520ef182f4170b1fd1cf42a
Rodrigo <rodrigostefano65@gmail.com>
2024-02-06 15:24:56 -0300

    Criação da base da aplicação

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { SingIn } from './pages/SingIn/index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SingIn />
+  </React.StrictMode>,
+)
---
+++ src/pages/SingIn/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+export function SingIn(){
+    return(
+        <Container>
+            <h1>
+                Rocketmoves
+            </h1>
+        </Container>
+    )
+}
